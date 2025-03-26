155e4e524a4889b87fb457f13ebb6299d2c7e009
sundara-raghav <g.sundararaghav2005@gmail.com>
2025-03-26 19:29:26 +0530

    second

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './Passwordgenerator';
 
 test('renders learn react link', () => {
   render(<App />);
